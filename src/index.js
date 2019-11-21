9af331b3762853ac7630d11e28d1b4ef343364d1
Matheus Cardoso <T-TI-07@Dev03.gestaotera.local>
2019-11-21 14:22:51 -0300

    repo init

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import './config/ReactotronConfig';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5fcff'
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10
+  }
+});
+
+console.tron.log('Hello World');
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Welcome to React Native!</Text>
+      <Text style={styles.welcome}>Texto</Text>
+    </View>
+  );
+}
