a4a27cae3ac208c484ae094bf475544c86a1aa1b
Matheus Cardoso <T-TI-07@Dev03.gestaotera.local>
2019-11-22 16:35:36 -0300

    Main page

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '192.168.0.176' })
+  const tron = Reactotron.configure({ host: '192.168.0.51' })
     .useReactNative()
     .connect();
 
--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StatusBar } from 'react-native';
 import './config/ReactotronConfig';
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  }
-});
-
-console.tron.log('Hello World');
+import Routes from './routes';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Welcome to React Native!</Text>
-      <Text style={styles.welcome}>Texto</Text>
-    </View>
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <Routes />
+    </>
   );
 }
